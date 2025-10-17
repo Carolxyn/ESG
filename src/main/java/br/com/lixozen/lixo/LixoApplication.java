579d67672015d0ee8fad86f14db4e2d47a3f841c
Carolina Caldas <carolcahh12345@gmail.com>
2025-10-17 20:12:27 -0300

    feat: adiciona endpoint de health

--- src/main/java/br/com/lixozen/lixo/LixoApplication.java
+++ src/main/java/br/com/lixozen/lixo/LixoApplication.java
@@ -2,13 +2,13 @@ package br.com.lixozen.lixo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.com.lixozen.lixo")type src\main\java\br\com\lixozen\lixo\controller\PontoColetaController.java
+@ComponentScan(basePackages = "br.com.lixozen.lixo")
 public class LixoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LixoApplication.class, args);
 	}
-
 }
